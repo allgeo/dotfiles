6addc83f739269f427a2411b0d6433d81f6df60e
allan <allgeocode@gmail.com>
2024-12-31 20:39:43 -0500

    kitty flatpack

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Copy entire content of the file with ctrl + shift + a
+vim.keymap.set("n", "<C-S-a>", ":%y+<CR>", { noremap = true, silent = true, desc = "Copy entire file to clipboard" })
